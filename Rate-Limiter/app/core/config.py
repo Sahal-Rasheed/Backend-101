90a3dfbb6cdea86f22e604e4179cea1476ff2c00
Sahal Rasheed <sahalrasheed48@gmail.com>
2026-04-14 23:51:20 +0530

    chore: refactor settings configuration to use SettingsConfigDict for environment variables

--- Rate-Limiter/app/core/config.py
+++ Rate-Limiter/app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -7,9 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     REDIS_URL: str = "redis://localhost:6379/2"
 
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 settings = Settings()
